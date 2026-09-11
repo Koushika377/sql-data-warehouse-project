e86a10e8ebcd483653b1366c3b63c725ebb6aa8e
Koushika377 <koushikayalam@gmail.com>
2026-09-11 16:21:39 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,34 +1,25 @@
 /*
-=======================================================================
 DDL Script: Create Gold Views
-=======================================================================
 This script creates views for Gold Layer in the data warehouse.
 The Gold layer represents final dimension and fact tables(Star Schema)
 
-Each view performs transformations and combines data from the silver
-layer to produce a clean, enriched, and business-ready dataset.
-
 These views can be queried directly for analytics and reporting.
-=======================================================================
 */
 
--- ====================================================================
 -- Create Dimension: gold.dim_customers
--- ====================================================================
-
 IF OBJECT_ID('gold.dim_customers','V') IS NOT NULL
-    DROP VIEW gold.dim_customers
+    DROP VIEW gold.dim_customers;
 GO
 CREATE VIEW gold.dim_customers AS
 SELECT 
-	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- surrogate key
 	ci.cst_id AS customer_id,
 	ci.cst_key AS customer_number,
 	ci.cst_firstname AS first_name,
 	ci.cst_lastname AS last_name,
 	la.cntry AS country,
 	ci.cst_marital_status AS marital_status,
-	CASE WHEN ci.cst_gndr!='n/a' THEN ci.cst_gndr --CRM is the master for gender info
+	CASE WHEN ci.cst_gndr!='n/a' THEN ci.cst_gndr 
 		ELSE COALESCE(ca.gen,'n/a')
 	END as gender,
 	ca.bdate AS birthdate,
@@ -37,8 +28,13 @@ FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
 ON ci.cst_key=ca.cid
 LEFT JOIN silver.erp_loc_a101 la
-ON ci.cst_key=la.cid
+ON ci.cst_key=la.cid;
+GO
 
+-- Create Dimension: gold.dim_products
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
 CREATE VIEW gold.dim_products AS
 SELECT 
 	ROW_NUMBER() OVER(ORDER BY pn.prd_start_dt,pn.prd_key) AS product_key,
@@ -55,9 +51,13 @@ SELECT
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
 ON pn.cat_id=pc.id
-WHERE prd_end_dt is null
+WHERE prd_end_dt is null;
+GO
 
-
+-- Create Fact Table: gold.fact_sales
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
 CREATE VIEW gold.fact_sales AS
 SELECT
 sd.sls_ord_num AS order_number,
@@ -73,4 +73,5 @@ FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
 ON sd.sls_prd_key=pr.product_number
 LEFT JOIN gold.dim_customers cu
-ON sd.sls_cust_id=cu.customer_id
+ON sd.sls_cust_id=cu.customer_id;
+GO

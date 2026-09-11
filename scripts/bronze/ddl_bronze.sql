a8ecc799bf379be8935ae609a423cdaa35fa2f65
Koushika377 <koushikayalam@gmail.com>
2026-09-11 16:13:25 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,8 +1,8 @@
 /*
 DDL Script: Create Bronze Tables
-  This script creates tables in the bronze schema, dropping existing tables
-  if they already exist
-Run this script to re-define the DDL structure of bronze tables
+
+This script creates tables in the bronze schema, dropping existing tables
+if they already exist
 */
 
 
